80f29bf1d6509ab2b85b9b0e18405ab151b161b2
yauravy <yauravy@gmail.com>
2023-11-06 23:20:03 +0100

    complete app

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,26 @@
+import Note from "@/components/Note"
+import prisma from "@/lib/db/prisma"
+import { auth } from "@clerk/nextjs"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "AllAi - Notes",
+}
+
+export default async function NotesPage(){
+    const {userId} = auth()
+
+    if(!userId) throw Error("userId undefined")
+
+    const allNotes = await prisma.note.findMany({where: {userId}})
+    return <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+        {allNotes.map((note) => (
+            <Note note={note} key={note.id} />
+        ))}
+        {allNotes.length === 0 && (
+            <div className="col-span-full text-center">
+                {"You don't have any note yet"}
+            </div>
+        )}
+    </div>
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  const { userId } = auth()
+
+  if(userId) redirect("/notes")
+  
+  return (
+    <main className="flex h-screen flex-col items-center justify-center gap-5">
+      <div className="flex items-center gap-4">
+        <Image src={logo} alt="logo image" width={100} height={100} />
+        <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          AllAi
+        </span>
+      </div>
+      <p className="max-w-prose text-center">
+        A note-taking app with Ai integration, built with OpenAi, Pinecone,
+        Next.js, Shadcn UI, Clerk
+      </p>
+      <Button size="lg" asChild>
+        <Link href="/notes">Open</Link>
+      </Button>
+    </main>
+  );
+}
